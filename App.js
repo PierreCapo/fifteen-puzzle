784216ca7a0529d99a04f68c7c9930a56c1bacbc
PierreCapo <pierre.caporossi@gadz.org>
2019-09-12 10:45:39 +0200

    Basic fifteen

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Transitioning } from "react-native-reanimated";
 
-export default function App() {
+const array = [1, 4, 6, 8, 2, 5, 9, 3, 15, 13, 7, 10, 12, 11, 14];
+const TILE_SIZE = 50;
+
+export default () => {
+  const transitioningRef = React.createRef();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Transitioning.View ref={transitioningRef} style={styles.transitioning}>
+        {array.map(tile => (
+          <View style={styles.tile} key={tile} />
+        ))}
+      </Transitioning.View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center"
   },
+  tile: {
+    height: TILE_SIZE,
+    width: TILE_SIZE,
+    backgroundColor: "purple",
+    borderWidth: 1
+  },
+  transitioning: {
+    width: TILE_SIZE * 4,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 64
+  }
 });
